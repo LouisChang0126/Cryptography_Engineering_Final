3a3ed70c2bc25e8230bc8a236bd45ea895e8e3b4
司徒立中 <rain.sytwu@gmail.com>
2024-06-06 16:28:31 +0800

    Update 3DES-CBC

--- analysis/3DES-CBC/3DES-CBC.ino
+++ analysis/3DES-CBC/3DES-CBC.ino
@@ -19,28 +19,33 @@ void setup() {
   printf_begin();  
   delay(100);  
 
-  printf("\n============================================\n");
-  printf(" Triple DES sample for Arduino/Raspberry pi\n");
-  printf("============================================\n");
-  des.init("012345677654321001234567\0",(unsigned long long int)0);
+  //printf("\n============================================\n");
+  //printf(" Triple DES sample for Arduino/Raspberry pi\n");
+  //printf("============================================\n");
+
+  for(int t=0;t<10;t++)
+  {
+      des.init("012345677654321001234567\0",(unsigned long long int)0);
+      ms = micros();
+      des.iv_inc();
+      byte plaintext[] = "Abstract We study the security of popular password managers and their policies on automatically filling in Web passwords. We examine browser built-in password managers, mobile password managers, and 3rd party managers. We observe significant differences in";
+      des.calc_size_n_pad(sizeof(plaintext));
+      byte plaintext_p[des.get_size()];
+      des.padPlaintext(plaintext,plaintext_p);
+      byte cyphertext[des.get_size()];
+      des.tdesCbcEncipher(plaintext_p,cyphertext);
+      //printf("Ciphertext in binary: ");
+      //printBinary(cyphertext, des.get_size());
+      
+      //printf(" Encryption took %lu micros\n",(micros()  - ms));
+      ms = micros();
+      des.calc_size_n_pad(sizeof(cyphertext));
+      des.tdesCbcDecipher(cyphertext,plaintext_p);
+      //printf(" Decryption took %lu micros\n\n",(micros() - ms));
+  }
+  Serial.println("Done");
 }
 
 void loop() {
-  ms = micros();
-  des.iv_inc();
-  byte plaintext[] = "Abstract Two kinds of contemporary developments in cryptography are examined. Widening applications of teleprocessing have given rise to a need for new types of cryptographic systems, which minimize the need for secure key distribution channels and supply ";
-  des.calc_size_n_pad(sizeof(plaintext));
-  byte plaintext_p[des.get_size()];
-  des.padPlaintext(plaintext,plaintext_p);
-  byte cyphertext[des.get_size()];
-  des.tdesCbcEncipher(plaintext_p,cyphertext);
-  //printf("Ciphertext in binary: ");
-  //printBinary(cyphertext, des.get_size());
-  
-  printf(" Encryption took %lu micros\n",(micros()  - ms));
-  ms = micros();
-  des.calc_size_n_pad(sizeof(cyphertext));
-  des.tdesCbcDecipher(cyphertext,plaintext_p);
-  printf(" Decryption took %lu micros\n\n",(micros() - ms));
-  delay(2000);
+
 }
