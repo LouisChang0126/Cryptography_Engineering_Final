20b6ec8861feda1d6b090f4f63d67c0c53698a4f
xuhe1004 <xuhe1010@gmail.com>
2024-06-06 16:55:13 +0800

    AES_analysis

--- analysis/AES/AES.ino
+++ analysis/AES/AES.ino
@@ -2,6 +2,7 @@
 
 #define BLOCK_SIZE 16
 #define KEY_LENGTH 32
+#define STRING_LENGTH 256
 
 uint8_t key[KEY_LENGTH] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 
                            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
@@ -9,11 +10,31 @@ uint8_t key[KEY_LENGTH] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
                            0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
 AESLib aesLib;
 
+
+// Function to generate a random byte array
+void generateRandomBytes(byte* buffer, size_t length) {
+  for (size_t i = 0; i < length; i++) {
+    buffer[i] = random(0, 256);
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   for(int t=0;t<100;t++)
   {
-      String input = "Abstract We study the security of popular password managers and their policies on automatically filling in Web passwords. We examine browser built-in password managers, mobile password managers, and 3rd party managers. We observe significant differences in";
+      randomSeed(analogRead(0));
+      byte randomBytes[STRING_LENGTH];
+      generateRandomBytes(randomBytes, STRING_LENGTH);
+
+      String hexString;
+      for (size_t i = 0; i < STRING_LENGTH; i++) {
+        if (randomBytes[i] < 16) {
+          hexString += "0"; // Add leading zero for single digit hex numbers
+        }
+        hexString += String(randomBytes[i], HEX);
+      }
+
+      String input = hexString;
       size_t len = input.length();
       if (len > 0) {
         uint8_t plaintext[len + 1];
